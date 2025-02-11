f7094957ac2ee862ea450715053e32b8e914751a
Dewinta Retno Puspitasari <dewintaretnop@gmail.com>
2025-02-11 15:11:19 +0700

    update css & js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 //File JS
+
+
+function konversi(event) {
+    event.preventDefault(); // Mencegah halaman reload
+    var celsius = document.getElementById("main-input").value;
+    var o = 9;
+    var i = 5
+    var a = 32;
+    var hasil = (celsius * o / i) + a;
+    
+document.getElementById("main-result").value = hasil.toFixed(2);
+
+document.getElementById("how-result").value = celsius + " × (9 : 5) + 32 = " + hasil.toFixed(2);
+}
+
+
+function resetForm(event) {
+    event.preventDefault(); 
+    document.getElementById("main-input").value = "";
+    document.getElementById("main-result").value = "";
+    document.getElementById("how-result").value = "";
+}
